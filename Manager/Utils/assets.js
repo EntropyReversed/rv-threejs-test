2d40da7982a7951731eb8c0ba5c393e3d5493da8
Stefan <atila35743@gmail.com>
2022-11-18 03:01:35 +0100

    test assets vercel

--- Manager/Utils/assets.js
+++ Manager/Utils/assets.js
@@ -7,11 +7,13 @@ export default [
   {
     name: 'model',
     type: 'glbModel',
-    path: './Public/rvModel2.glb',
+    // path: '/Public/rvModel2.glb',
+    path: '../../3dassets/rvModel2.glb',
   },
   {
     name: 'gradientTexture',
     type: 'image',
-    path: './Public/texture0.png',
+    // path: '/Public/texture0.png',
+    path: '../../3dassets/texture0.png',
   },
 ];
